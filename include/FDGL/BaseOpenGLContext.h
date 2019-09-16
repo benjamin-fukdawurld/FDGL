4a313415be7bd33db070d0cfa8347e7cbcf310d0
Benjamin ZAMOUR <benjamin.zamour.ext@parrot.com>
2019-09-16 18:14:01 +0200

    Added glEnable features on context + common enable helper

--- include/FDGL/BaseOpenGLContext.h
+++ include/FDGL/BaseOpenGLContext.h
@@ -1,6 +1,8 @@
 #ifndef BASEOPENGLCONTEXT_H
 #define BASEOPENGLCONTEXT_H
 
+#include <cstdint>
+
 namespace FDGL
 {
     class BaseOpenGLWindow;
@@ -13,6 +15,31 @@ namespace FDGL
         public:
             BaseOpenGLContext();
             virtual ~BaseOpenGLContext();
+
+            void enableDepth();
+            bool isDepthEnabled() const;
+            void disableDepth();
+
+            void enableBlend();
+            bool isBlendEnabled() const;
+            void disableBlend();
+
+            void enableFaceCulling();
+            bool isFaceCullingEnabled() const;
+            void disableFaceCulling();
+
+            void enableMultiSampling();
+            bool isMultiSamplingEnabled() const;
+            void disableMultiSampling();
+
+            void enable(uint32_t capability);
+            void enable(uint32_t capability, uint32_t index);
+
+            bool isEnabled(uint32_t capability) const;
+            bool isEnabled(uint32_t capability, uint32_t index) const;
+
+            void disable(uint32_t capability);
+            void disable(uint32_t capability, uint32_t index);
 
             virtual bool loadOpenGLFunctions(GetProcAddressFunc f) const;
 
--- src/BaseOpenGLContext.cpp
+++ src/BaseOpenGLContext.cpp
@@ -8,6 +8,96 @@ FDGL::BaseOpenGLContext::BaseOpenGLContext() {}
 
 FDGL::BaseOpenGLContext::~BaseOpenGLContext() {}
 
+void FDGL::BaseOpenGLContext::enableDepth()
+{
+    enable(GL_DEPTH);
+}
+
+bool FDGL::BaseOpenGLContext::isDepthEnabled() const
+{
+    return isEnabled(GL_DEPTH);
+}
+
+void FDGL::BaseOpenGLContext::disableDepth()
+{
+    disable(GL_DEPTH);
+}
+
+void FDGL::BaseOpenGLContext::enableBlend()
+{
+    enable(GL_BLEND);
+}
+
+bool FDGL::BaseOpenGLContext::isBlendEnabled() const
+{
+    return isEnabled(GL_BLEND);
+}
+
+void FDGL::BaseOpenGLContext::disableBlend()
+{
+    disable(GL_BLEND);
+}
+
+void FDGL::BaseOpenGLContext::enableFaceCulling()
+{
+    enable(GL_CULL_FACE);
+}
+
+bool FDGL::BaseOpenGLContext::isFaceCullingEnabled() const
+{
+    return isEnabled(GL_CULL_FACE);
+}
+
+void FDGL::BaseOpenGLContext::disableFaceCulling()
+{
+    disable(GL_CULL_FACE);
+}
+
+void FDGL::BaseOpenGLContext::enableMultiSampling()
+{
+    enable(GL_MULTISAMPLE);
+}
+
+bool FDGL::BaseOpenGLContext::isMultiSamplingEnabled() const
+{
+    return isEnabled(GL_MULTISAMPLE);
+}
+
+void FDGL::BaseOpenGLContext::disableMultiSampling()
+{
+    disable(GL_MULTISAMPLE);
+}
+
+void FDGL::BaseOpenGLContext::enable(uint32_t capability)
+{
+    glEnable(static_cast<GLenum>(capability));
+}
+
+void FDGL::BaseOpenGLContext::enable(uint32_t capability, uint32_t index)
+{
+    glEnablei(static_cast<GLenum>(capability), static_cast<GLuint>(index));
+}
+
+bool FDGL::BaseOpenGLContext::isEnabled(uint32_t capability) const
+{
+    return glIsEnabled(static_cast<GLenum>(capability)) == GL_TRUE;
+}
+
+bool FDGL::BaseOpenGLContext::isEnabled(uint32_t capability, uint32_t index) const
+{
+    return glIsEnabledi(static_cast<GLenum>(capability), static_cast<GLuint>(index)) == GL_TRUE;
+}
+
+void FDGL::BaseOpenGLContext::disable(uint32_t capability)
+{
+    glDisable(static_cast<GLenum>(capability));
+}
+
+void FDGL::BaseOpenGLContext::disable(uint32_t capability, uint32_t index)
+{
+    glDisablei(static_cast<GLenum>(capability), static_cast<GLuint>(index));
+}
+
 bool FDGL::BaseOpenGLContext::loadOpenGLFunctions(FDGL::BaseOpenGLContext::GetProcAddressFunc f) const
 {
     if(!gladLoadGLLoader(f))
